46032c8cb72111f7d7d082279d839bf3ee0f28dc
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-01-27 19:37:26 -0500

    Prueba3

---
+++ src/main/java/com/example/demo/EjercicioU2PaP3JaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioU2PaP3JaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioU2PaP3JaApplication.class, args);
+	}
+
+}
